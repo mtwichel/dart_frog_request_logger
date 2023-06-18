413efd847e8cb30494d3292a068769e559d04fb3
Marcus Twichel <marc.twichy@gmail.com>
2023-06-17 22:01:29 -0600

    fix github actions

---
+++ example/test/routes/index_test.dart
@@ -0,0 +1,23 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:request_logger/request_logger.dart';
+import 'package:test/test.dart';
+
+import '../../routes/index.dart' as route;
+
+class _MockRequestContext extends Mock implements RequestContext {}
+
+class _MockRequestLogger extends Mock implements RequestLogger {}
+
+void main() {
+  group('GET /', () {
+    test('Logs the message', () {
+      final context = _MockRequestContext();
+      final logger = _MockRequestLogger();
+      when(() => context.read<RequestLogger>()).thenReturn(logger);
+      route.onRequest(context);
+
+      verify(() => logger.debug('Hello Logs')).called(1);
+    });
+  });
+}
